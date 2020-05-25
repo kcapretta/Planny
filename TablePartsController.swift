955032c5205dbf9305fd5ae34bd948688e5f144e
Kassandra <46635962+kcapretta@users.noreply.github.com>
2020-05-25 16:44:19 -0400

    Create TablePartsController.swift

---
+++ TablePartsController.swift
@@ -0,0 +1,19 @@
+//
+//  TableBodyPartsController.swift
+//  Planny WatchKit Extension
+//
+//  Created by Kassandra Capretta on 2/17/20.
+//  Copyright © 2020 Kassandra Capretta. All rights reserved.
+//
+
+import WatchKit
+
+class TablePartsController: NSObject {
+
+    //MARK:- Interface Builder
+    @IBOutlet weak var allDataLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var timeLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var imageData: WKInterfaceImage!
+}
