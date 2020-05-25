ebfa4f8e114bb93c16addf1b22c9687b12e5d63a
Kassandra <46635962+kcapretta@users.noreply.github.com>
2020-05-25 16:45:01 -0400

    Create CellController.swift

---
+++ CellController.swift
@@ -0,0 +1,18 @@
+//
+//  CellController.swift
+//  Planny WatchKit Extension
+//
+//  Created by Kassandra Capretta on 2/25/20.
+//  Copyright © 2020 Kassandra Capretta. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class CellController: NSObject {
+
+    @IBOutlet weak var optionLabel: WKInterfaceLabel!
+
+    @IBOutlet weak var tableNumber: WKInterfaceLabel!
+}
